f5f588c5dcc3061ff4cf6056d0822d07a1a91adf
Dorgival R <dojak220@ufrn.edu.br>
2020-05-25 15:21:26 -0300

    frontend do app

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -4,55 +4,54 @@ const connection = require('../database/connection');
 const LIMIT_PAGE = 5
 
 module.exports = {
-    async index(request, response) {
-        const {page = 1} = request.query;
-        const [count] = await connection('incidents').count();
+   async index(request, response) {
+      const { page = 1 } = request.query;
+      const [count] = await connection('incidents').count();
 
-        const incidents = await connection('incidents')
-            .join('ongs','ongs.id','=','incidents.ong_id')
-            .limit(LIMIT_PAGE)
-            .offset((page - 1) * LIMIT_PAGE)
-            .select([
-                'incidents.*',
-                'ongs.nome',
-                'ongs.email',
-                'ongs.whatsapp',
-                'ongs.cidade',
-                'ongs.uf'
-            ]);
+      const incidents = await connection('incidents')
+         .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+         .limit(LIMIT_PAGE)
+         .offset((page - 1) * LIMIT_PAGE)
+         .select([
+            'incidents.*',
+            'ongs.nome',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.cidade',
+            'ongs.uf'
+         ]);
 
-        response.header('X-Total-Count', count['count(*)']);
-        return response.json(incidents);
-    },
+      response.header('X-Total-Count', count['count(*)']);
+      return response.json(incidents);
+   },
 
-    async create(request, response) {
-        const {titulo, descricao, valor} = request.body;
-        const ong_id = request.headers.authorization
+   async create(request, response) {
+      const { titulo, descricao, valor } = request.body;
+      const ong_id = request.headers.authorization
+      console.log({ong_id})
+      const [id] = await connection('incidents').insert({
+         titulo,
+         descricao,
+         valor,
+         ong_id,
+      });
+      return response.json({ id }) // vai retornar o id do caso cadastrado no sistema.
+   },
 
-        const [id] = await connection('incidents').insert({
-            titulo,
-            descricao,
-            valor,
-            ong_id,
-        });
-        return response.json({id}) // vai retornar o id do caso cadastrado no sistema.
-    },
+   async delete(request, response) {
+      const { id } = request.params;
+      const ong_id = request.headers.authorization
+      const incident = await connection('incidents')
+         .where('id', id)
+         .select('ong_id')
+         .first();
 
-    async delete(request, response) {
-        const {id} = request.params;
-        const ong_id = request.headers.authorization
+      if (incident.ong_id !== ong_id) {
+         return response.status(401).json({ error: 'Operação não permitida' });
+      }
 
-        const incident = await connection('incidents')
-            .where('id', id)
-            .select('ong_id')
-            .first();
+      await connection('incidents').where('id', id).delete();
 
-        if(incident.ong_id != ong_id){
-            return response.status(401).json({error: 'Operação não permitida'});
-        }
-
-        await connection('incidents').select('id', id).delete();
-
-        return response.status(204).send(); //resposta sem conteúdo, normalmente usada quando a operação teve sucesso, mas não há resposta. O send() envia uma página em branco.
-    },
+      return response.status(204).send(); //resposta sem conteúdo, normalmente usada quando a operação teve sucesso, mas não há resposta. O send() envia uma página em branco.
+   },
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Include Engenharia
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
 export default App;
+
+
+
+
+
+/*Como estava antes, para entender alguns conceitos
+
+
+import React, {useState} from 'react';
+import Header from './Header';
+
+// JSX, que significa que temos elementos de HTML dentro de uma estrutura JS
+
+function App() {
+  const [counter, setCounter] = useState(0); //Não pode ser uma variável comum. Deve ter essa forma.
+
+  // Array [valor, função de atualização do valor]
+
+  function increment(){
+      setCounter(counter + 1) //count++;
+  }
+  
+  return (
+    <div>
+      <Header>Contador = {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+  );
+}
+
+
+export default App;
+
+*/
